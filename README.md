abbd6cf80f5d0ab5ca95db0c42c4bf426c308f51
Bob Paauwe <bpaauwe@yahoo.com>
2022-04-22 11:03:06 -0700

    Initial port to Polyglot V3.

--- README.md
+++ README.md
@@ -1,17 +1,8 @@
-# UDI Polyglot v2 SUn Poly
+# SUn Poly
 
 [![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://github.com/exking/udi-sun-poly/blob/master/LICENSE)
 
-This Poly provides ability to track Sun position for [Polyglot v2](https://github.com/UniversalDevicesInc/polyglot-v2) server.
-
-### Installation instructions
-You can install NodeServer from the Polyglot store or manually running
-```
-cd ~/.polyglot/nodeservers
-git clone https://github.com/exking/udi-sun-poly.git Sun
-cd Sun
-./install.sh
-```
+This Poly provides ability to track Sun position for [Polyglot v3](https://github.com/UniversalDevicesInc/polyglot-v2).
 
 ### Configuration
 `longitude` and `latitude` are the two mandatory configuration parameters.
--- sun-poly.py
+++ sun-poly.py
@@ -1,52 +1,93 @@
 #!/usr/bin/env python3
 
-import polyinterface
+import udi_interface
 import sys
 from astral.location import Location
 from tzlocal import get_localzone
 import datetime
 
-LOGGER = polyinterface.LOGGER
+LOGGER = udi_interface.LOGGER
 
 
-class Controller(polyinterface.Controller):
-    def __init__(self, polyglot):
-        super().__init__(polyglot)
-        self.name = 'Sun Position'
-        self.address = 'sunctrl'
-        self.primary = self.address
+class Controller(udi_interface.Node ):
+    def __init__(self, polyglot, primary, address, name):
+        super().__init__(polyglot, primary, address, name)
+        self.poly = polyglot
+        self.name = name
+        self.address = address
+        self.primary = primary
         self.location = None
         self.tz = None
         self.today = None
         self.sunrise = None
         self.sunset = None
         self.sun_above_horizon = False
+        self.configured = False
 
-    def start(self):
-        LOGGER.info('Started Sun Position')
-        if not 'longitude' in self.polyConfig['customParams'] or not 'latitude' in self.polyConfig['customParams']:
-            LOGGER.error('Please specify latitude and longitude configuration parameters')
+        polyglot.subscribe(polyglot.START, self.start, address)
+        polyglot.subscribe(polyglot.CUSTOMPARAMS, self.parameterHandler)
+        polyglot.subscribe(polyglot.POLL, self.poll)
+
+        polyglot.ready()
+        polyglot.addNode(self, conn_status="ST")
+
+
+    def parameterHandler(self, params):
+        self.poly.Notices.clear()
+        self.configured = False
+        valid = True
+
+        if 'longitude' in params:
+            if params['longitude'] == '':
+                valid = False
+                self.poly.Notices['long'] = 'Please specify longitude in configuraton parameters'
+            else:
+                self.location.longitude = float(params['longitude'])
         else:
+            self.poly.Notices['long'] = 'Please specify longitude in configuraton parameters'
+            valid = False
+
+        if 'latitude' in params:
+            if params['latitude'] == '':
+                valid = False
+                self.poly.Notices['lat'] = 'Please specify latitude in configuraton parameters'
+            else:
+                self.location.latitude = float(params['latitude'])
+        else:
+            self.poly.Notices['lat'] = 'Please specify latitude in configuraton parameters'
+            valid = False
+
+        if 'elevation' in params:
+            self.location.elevation = int(params['elevation'])
+        else:
+            self.location.elevation = 0
+
+        if valid:
             self.tz = get_localzone()
             self.today = datetime.date.today()
             self.location = Location()
             self.location.timezone = str(self.tz)
-            self.location.longitude = float(self.polyConfig['customParams']['longitude'])
-            self.location.latitude = float(self.polyConfig['customParams']['latitude'])
-            if 'elevation' in self.polyConfig['customParams']:
-                self.location.elevation = int(self.polyConfig['customParams']['elevation'])
             self.sunrise = self.location.sunrise()
             self.sunset = self.location.sunset()
             ts_now = datetime.datetime.now(self.tz)
             if self.sunrise < ts_now < self.sunset:
                 self.sun_above_horizon = True
-            self.updateInfo()
+            self.configured = True
+
+    def start(self):
+        LOGGER.info('Started Sun Position')
+
+        while not self.configured:
+            sleep(1000)
+
+       self.updateInfo()
 
     def stop(self):
         LOGGER.info('Sun Position is stopping')
 
-    def shortPoll(self):
-        self.updateInfo()
+    def poll(self, pollflag):
+        if pollflag == 'shortPoll':
+            self.updateInfo()
             
     def updateInfo(self):
         if self.location is None:
@@ -75,12 +116,11 @@ class Controller(polyinterface.Controller):
                 self.sun_above_horizon = False
 
     def query(self):
-        for node in self.nodes:
-            self.nodes[node].reportDrivers()
+        self.reportDrivers()
 
     id = 'SUNCTRL'
     commands = {'QUERY': query}
-    drivers = [{'driver': 'ST', 'value': 1, 'uom': 2},
+    drivers = [{'driver': 'ST', 'value': 0, 'uom': 25},
                {'driver': 'GV0', 'value': 0, 'uom': 14},
                {'driver': 'GV1', 'value': 0, 'uom': 14},
                {'driver': 'GV2', 'value': 0, 'uom': 14},
@@ -90,9 +130,9 @@ class Controller(polyinterface.Controller):
 
 if __name__ == "__main__":
     try:
-        polyglot = polyinterface.Interface('Sun')
-        polyglot.start()
-        control = Controller(polyglot)
-        control.runForever()
+        polyglot = udi_interface.Interface([])
+        polyglot.start('0.0.3')
+        Controller(polyglot, 'sunctrl', 'sunctrl', 'Sun Position')
+        polyglot.runForever()
     except (KeyboardInterrupt, SystemExit):
         sys.exit(0)
